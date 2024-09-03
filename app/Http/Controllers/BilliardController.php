30f63953405d3945358c93cebbebe22846de268f
michaelblendmedia <michael@blendmedia.co.id>
2024-09-03 15:22:05 +0700

    Update BilliardController.php

--- app/Http/Controllers/BilliardController.php
+++ app/Http/Controllers/BilliardController.php
@@ -216,7 +216,6 @@ class BilliardController extends Controller
             // Total biaya keseluruhan
             $total = $mejatotal + $total_makanan;
             $total = round($total); 
-            return $lama_waktu;
             return view('invoice.stop', compact('meja_rental','meja_rental2', 'no_meja', 'rental', 'lama_waktu', 'mejatotal', 'total', 'makanan'));
         } else {
             return redirect()->back()->with('error', 'No rental found for the specified table.');
