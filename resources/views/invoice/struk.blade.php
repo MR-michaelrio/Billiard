08a77ab627eb9219c00106333d1ef92a41c2927a
michaelblendmedia <michael@blendmedia.co.id>
2024-09-05 17:25:46 +0700

    update

--- resources/views/invoice/struk.blade.php
+++ resources/views/invoice/struk.blade.php
@@ -135,7 +135,7 @@
                         </tr>
                     @endforeach
                 @endforeach
-                <span id="idtable">{{$invoice->id_belanja}}</span>
+                <span id="data-idtable">{{$invoice->id_belanja}}</span>
         </table>
 
         <!-- Totals Section -->
@@ -155,35 +155,35 @@
         window.addEventListener('load', function() {
             window.print();
         });
-        const idtable = document.querySelector('[data-idtable]').getAttribute('data-idtable');
-        console.log('ID Table:', idtable); // For debugging
-        window.addEventListener('afterprint', function() {
-            
 
-            fetch('{{ route("print.status") }}', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                },
-                body: JSON.stringify({ id_table: idtable })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Response data:', data); // For debugging
-                if (data.success) {
-                    // Perform redirection if success
-                    const redirectUrl = '{{ route("bl.index") }}';
-                    window.location.href = redirectUrl;
-                } else {
-                    alert('There was an error updating the status');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('There was an error during the status update. Please check the console for more details.');
-            });
-        });
+        // window.addEventListener('afterprint', function() {
+        //     const idtable = document.querySelector('[data-idtable]').getAttribute('data-idtable');
+        //     console.log('ID Table:', idtable); // For debugging
+
+        //     fetch('{{ route("print.status") }}', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //             'X-CSRF-TOKEN': '{{ csrf_token() }}'
+        //         },
+        //         body: JSON.stringify({ id_table: idtable })
+        //     })
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log('Response data:', data); // For debugging
+        //         if (data.success) {
+        //             // Perform redirection if success
+        //             const redirectUrl = '{{ route("bl.index") }}';
+        //             window.location.href = redirectUrl;
+        //         } else {
+        //             alert('There was an error updating the status');
+        //         }
+        //     })
+        //     .catch(error => {
+        //         console.error('Error:', error);
+        //         alert('There was an error during the status update. Please check the console for more details.');
+        //     });
+        // });
 
     </script>
 </body>
