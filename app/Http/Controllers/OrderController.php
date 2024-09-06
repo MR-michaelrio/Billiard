6891805567ab0902dde87a6527e41293a3bb0056
michaelblendmedia <michael@blendmedia.co.id>
2024-09-07 02:10:00 +0700

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -162,7 +162,8 @@ class OrderController extends Controller
 
     public function detailorder($id){
         $orderItems = OrderItem::where($id,"order_id")->get();
-        return view("invoice.detail-order", compact("orderItems"));
+        return $orderItems;
+        // return view("invoice.detail-order", compact("orderItems"));
     }
     
 }
