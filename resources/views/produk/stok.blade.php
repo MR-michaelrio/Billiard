027f76d356de9e3dd71fedcef6ef8954b767d926
michaelblendmedia <michael@blendmedia.co.id>
2024-09-12 23:06:08 +0700

    Update stok.blade.php

--- resources/views/produk/stok.blade.php
+++ resources/views/produk/stok.blade.php
@@ -9,7 +9,7 @@
             <a href="{{route('produk.create')}}" class="btn btn-primary">Tambah Produk</a>
             <!-- /.card-header -->
             <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
+                <table id="example2" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>Id Produk</th>
