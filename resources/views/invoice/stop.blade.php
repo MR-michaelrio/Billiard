97601af7ae9f80a2b476ff0c09d9f3d21443aeb5
michaelblendmedia <michael@blendmedia.co.id>
2024-09-12 21:37:08 +0700

    Update stop.blade.php

--- resources/views/invoice/stop.blade.php
+++ resources/views/invoice/stop.blade.php
@@ -91,7 +91,7 @@
                 @foreach($meja_rental2 as $r)
                 <button type="button" class="submit-button btn btn-success float-right" data-metode="Cash" data-rental="{{ $r->id_rental }}" data-meja="{{ $r->no_meja }}">
                     Cash Payment {{ $r->no_meja }}
-                </button>
+                </button>&nbsp;|&nbsp;
                 <button type="button" class="submit-button btn btn-success float-right" data-metode="Transfer" data-rental="{{ $r->id_rental }}" data-meja="{{ $r->no_meja }}">
                     Transfer Payment {{ $r->no_meja }}
                 </button>
