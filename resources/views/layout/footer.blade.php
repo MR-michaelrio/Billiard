3d74567770b7c1290c40feac7d6b689961ddbff9
michaelblendmedia <michael@blendmedia.co.id>
2024-09-12 23:04:27 +0700

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -105,7 +105,7 @@ $('#reservationdate').datetimepicker({
     $("#example2").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example2_wrapper .col-md-6:eq(0)');
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
       "paging": true,
       "lengthChange": false,
