7969dc07ae1bc4119685f9c0db934f07075f2c1a
michaelblendmedia <michael@blendmedia.co.id>
2024-09-13 07:16:55 +0700

    Update BilliardController.php

--- app/Http/Controllers/BilliardController.php
+++ app/Http/Controllers/BilliardController.php
@@ -661,7 +661,7 @@ class BilliardController extends Controller
         }
     
         // Return the view with the summarized data
-        return view('invoice.rekap-order', compact('data'));
+        return view('invoice.rekap-table', compact('data'));
     }
     
     
