88f5eb03afe9bdb014877fa0ad6b842f2732751e
michaelblendmedia <michael@blendmedia.co.id>
2024-09-27 21:12:14 +0700

    rekap

--- resources/views/invoice/rekap-bulan.blade.php
+++ resources/views/invoice/rekap-bulan.blade.php
@@ -4,7 +4,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Rekap</h3>
+                <h3 class="card-title">Rekap Bulanan</h3>
             </div>               
             <!-- /.card-header -->
             <div class="card-body">
