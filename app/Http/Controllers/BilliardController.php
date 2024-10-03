13a17b53afd6c3ae04a398b5800415815661386a
michaelblendmedia <michael@blendmedia.co.id>
2024-10-03 16:53:19 +0700

    Update BilliardController.php

--- app/Http/Controllers/BilliardController.php
+++ app/Http/Controllers/BilliardController.php
@@ -909,7 +909,6 @@ class BilliardController extends Controller
         $rekaps = DB::table('invoice')
             ->join('rental_invoice', 'invoice.id_rental', '=', 'rental_invoice.id_rental')
             ->join('orders', 'invoice.id_belanja', '=', 'orders.id')
-            ->where(DB::raw('MONTH(invoice.created_at)'), $bulan)
             ->where(DB::raw('MONTH(orders.created_at)'), $bulan)
             ->get();
             
